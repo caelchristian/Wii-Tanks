8712fd6ac77f1de5c7077a034aae989095df04bf
levip <leviptm@gmail.com>
2022-10-05 14:58:08 -0400

    added background image

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        arcade.set_background_color(arcade.color.AMAZON)
+        self.background = None
 
         # If you have sprite lists, you should create them here,
         # and set them to None
 
     def setup(self):
         # Create your sprites and sprite lists here
-        pass
+         self.background = arcade.load_texture("background.jpg")
 
     def on_draw(self):
         """
@@ -36,6 +36,11 @@ class MyGame(arcade.Window):
         # This command should happen before we start drawing. It will clear
         # the screen to the background color, and erase what we drew last frame.
         arcade.start_render()
+
+        # Draw the background texture
+        arcade.draw_lrwh_rectangle_textured(0, 0,
+                                            SCREEN_WIDTH, SCREEN_HEIGHT,
+                                            self.background)
 
         # Call draw() on all your sprite lists below
 
